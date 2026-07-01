d9da7f5d26864d44c3889f1de2848dd2f700b5d9
AQIBMOHD <aqibmohammad.0002@gmail.com>
2026-07-01 13:21:13 +0530

    configure remote Backend in provider.tf

--- provider.tf
+++ provider.tf
@@ -5,8 +5,18 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  # now here i add Azure Backend where I store terraform.tfstate file which i created maually
+  backend "azurerm"{
+    resource_group_name = "rg-terraform-state"
+    storage_account_name = "aqibtfstate2000"
+    container_name = "tfstate"
+    key = "dev.terraform.tfstate"
+  }
+  
 }
 
 provider "azurerm" {
   features {}
 }
+
